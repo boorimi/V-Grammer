95c06822a1f8e85cb1942ec5998cf5a9fc4a037f
JABIRAN <ds6951@naver.com>
2024-06-06 09:56:07 +0900

    [regular commit] update main page video
    메인페이지 비디오 띄우기 거의다 완성

--- src/main/java/com/vg/kw/main/HC.java
+++ src/main/java/com/vg/kw/main/HC.java
@@ -12,12 +12,13 @@ public class HC extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HomeDAO.HDAO.getLiveUrl(request);
+//		HomeDAO.HDAO.getLiveUrl(request);
+//		GetAllStream.getAllLive();
 		request.setAttribute("content", "mainpage/main.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
