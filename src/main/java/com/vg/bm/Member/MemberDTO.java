44dea89b554cdb30c99ecaa571ab613a918044a4
boorimi <qnfal123@gmail.com>
2024-06-07 13:02:16 +0900

    [EDIT] # > %23 replace, <a>로 트위터 해시태그 페이지로 넘어갈 수 있게 작업ㅊ

--- src/main/java/com/vg/bm/Member/MemberDTO.java
+++ src/main/java/com/vg/bm/Member/MemberDTO.java
@@ -10,6 +10,7 @@ public class MemberDTO {
 	private String m_debut;
 	private String m_mother_name;
 	private String m_mother_twitter;
+	private String m_introduce;
 	private String i_icon;
 	private String i_img;
 	private String i_3side;
@@ -17,41 +18,8 @@ public class MemberDTO {
 	private ArrayList<AddressDTO> address;
 	private ArrayList<HashTagDTO> hashTag;
 
-	public MemberDTO(String m_pk, String m_name, String m_gen, String m_birth, String m_debut, String m_mother_name,
-			String m_mother_twitter, String i_icon, String i_img, String i_3side, String i_background
-			) {
-		super();
-		this.m_pk = m_pk;
-		this.m_name = m_name;
-		this.m_gen = m_gen;
-		this.m_birth = m_birth;
-		this.m_debut = m_debut;
-		this.m_mother_name = m_mother_name;
-		this.m_mother_twitter = m_mother_twitter;
-		this.i_icon = i_icon;
-		this.i_img = i_img;
-		this.i_3side = i_3side;
-		this.i_background = i_background;
-	}
-
 	public MemberDTO() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public ArrayList<AddressDTO> getAddress() {
-		return address;
-	}
-
-	public void setAddress(ArrayList<AddressDTO> address) {
-		this.address = address;
-	}
-
-	public ArrayList<HashTagDTO> getHashTag() {
-		return hashTag;
-	}
-
-	public void setHashTag(ArrayList<HashTagDTO> hashTag) {
-		this.hashTag = hashTag;
 	}
 
 	public String getM_pk() {
@@ -110,6 +78,14 @@ public class MemberDTO {
 		this.m_mother_twitter = m_mother_twitter;
 	}
 
+	public String getM_introduce() {
+		return m_introduce;
+	}
+
+	public void setM_introduce(String m_introduce) {
+		this.m_introduce = m_introduce;
+	}
+
 	public String getI_icon() {
 		return i_icon;
 	}
@@ -142,12 +118,49 @@ public class MemberDTO {
 		this.i_background = i_background;
 	}
 
+	public ArrayList<AddressDTO> getAddress() {
+		return address;
+	}
+
+	public void setAddress(ArrayList<AddressDTO> address) {
+		this.address = address;
+	}
+
+	public ArrayList<HashTagDTO> getHashTag() {
+		return hashTag;
+	}
+
+	public void setHashTag(ArrayList<HashTagDTO> hashTag) {
+		this.hashTag = hashTag;
+	}
+
+	public MemberDTO(String m_pk, String m_name, String m_gen, String m_birth, String m_debut, String m_mother_name,
+			String m_mother_twitter, String m_introduce, String i_icon, String i_img, String i_3side,
+			String i_background, ArrayList<AddressDTO> address, ArrayList<HashTagDTO> hashTag) {
+		super();
+		this.m_pk = m_pk;
+		this.m_name = m_name;
+		this.m_gen = m_gen;
+		this.m_birth = m_birth;
+		this.m_debut = m_debut;
+		this.m_mother_name = m_mother_name;
+		this.m_mother_twitter = m_mother_twitter;
+		this.m_introduce = m_introduce;
+		this.i_icon = i_icon;
+		this.i_img = i_img;
+		this.i_3side = i_3side;
+		this.i_background = i_background;
+		this.address = address;
+		this.hashTag = hashTag;
+	}
+
 	@Override
 	public String toString() {
 		return "MemberDTO [m_pk=" + m_pk + ", m_name=" + m_name + ", m_gen=" + m_gen + ", m_birth=" + m_birth
 				+ ", m_debut=" + m_debut + ", m_mother_name=" + m_mother_name + ", m_mother_twitter=" + m_mother_twitter
-				+ ", i_icon=" + i_icon + ", i_img=" + i_img + ", i_3side=" + i_3side + ", i_background=" + i_background
-				+ ", address=" + address + ", hashTag=" + hashTag + "]";
+				+ ", m_introduce=" + m_introduce + ", i_icon=" + i_icon + ", i_img=" + i_img + ", i_3side=" + i_3side
+				+ ", i_background=" + i_background + ", address=" + address + ", hashTag=" + hashTag + "]";
 	}
-
+	
+	
 }
