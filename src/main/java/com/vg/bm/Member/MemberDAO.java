48cb2f9f567ec2a107769465f7b680c1a30683f4
boorimi <qnfal123@gmail.com>
2024-06-07 20:45:08 +0900

    [EDIT] member css // twitter > X replace,

--- src/main/java/com/vg/bm/Member/MemberDAO.java
+++ src/main/java/com/vg/bm/Member/MemberDAO.java
@@ -34,11 +34,8 @@ public class MemberDAO {
 		ResultSet rs3 = null;
 		// 멤버와 멤버이미지 테이블을 보여주는 sql문 한 번에 작성.
 		String sql = "select hm.m_pk, hm.m_name,hm.m_gen,hm.m_birth,hm.m_debut,hm.m_mother_name,\r\n"
-				+ "hm.m_mother_twitter,hm.m_introduce,\r\n"
-				+ "hi.i_icon,hi.i_img,hi.i_3side,hi.i_background\r\n"
-				+ "from haco_member hm, haco_image hi\r\n"
-				+ "where hm.m_pk = hi.i_m_pk\r\n"
-				+ "order by hm.m_pk;";
+				+ "hm.m_mother_twitter,hm.m_introduce,\r\n" + "hi.i_icon,hi.i_img,hi.i_3side,hi.i_background\r\n"
+				+ "from haco_member hm, haco_image hi\r\n" + "where hm.m_pk = hi.i_m_pk\r\n" + "order by hm.m_pk;";
 		try {
 			con = DBManager.connect();
 			pstmt = con.prepareStatement(sql);
@@ -49,7 +46,7 @@ public class MemberDAO {
 			ArrayList<AddressDTO> addrs = null;
 			HashTagDTO hashTagDTO = null;
 			ArrayList<HashTagDTO> hashTag = null;
-			
+
 			// string으로 저장된 생일날짜를 date(MM-dd) 형식으로 바꿔주는 코드(하단 첫번째 try catch문 까지)
 			SimpleDateFormat oldBirth = new SimpleDateFormat("yyyy-MM-dd");
 			SimpleDateFormat newBirth = new SimpleDateFormat("MM-dd");
@@ -61,36 +58,40 @@ public class MemberDAO {
 				String birth = rs.getString(4);
 				Date birth2 = null;
 				String birth3 = "";
-				
+
 				try {
 					birth2 = oldBirth.parse(birth);
 					birth3 = newBirth.format(birth2);
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 				// 멤버와 멤버이미지 테이블의 정보를 합쳐서 MemberDTO에 생성.
-				MemberDTO member = new MemberDTO(rs.getString(1),rs.getString(2),
-						rs.getString(3), birth3 ,rs.getString(5),rs.getString(6),
-						rs.getString(7),rs.getString(8),rs.getString(9),
-						rs.getString(10),rs.getString(11),rs.getString(12),
-						addrs, hashTag);
-																	// 멤버 pk
+				MemberDTO member = new MemberDTO(rs.getString(1), rs.getString(2), rs.getString(3), birth3,
+						rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9),
+						rs.getString(10), rs.getString(11), rs.getString(12), addrs, hashTag);
+				// 멤버 pk
 				sql = "select * from haco_address where a_m_pk=" + rs.getString(1);
 				pstmt = con.prepareStatement(sql);
 				rs2 = pstmt.executeQuery();
-				
+
 				// 멤버 address 테이블 정보 추가
 				addrs = new ArrayList<AddressDTO>();
 				while (rs2.next()) {
 					// 어드레스 카테고리 중 category가 'YchannelID' 이거나 'YuploadPLID' 인 경우 출력 X
-					String addr = rs2.getString(3);
-					if (!addr.equals("YchannelID") && !addr.equals("YuploadPLID")) {
-						addressDTO = new AddressDTO(rs2.getString(1), addr, rs2.getString(4));
+					String category = rs2.getString(3);
+					if (!category.equals("YchannelID") && !category.equals("YuploadPLID")) {
+						addressDTO = new AddressDTO(rs2.getString(1), category, rs2.getString(4));
+						// twitter > X replace
+						if (category.equals("twitter")) {
+							category = category.replace("twitter", "X");
+							addressDTO = new AddressDTO(rs2.getString(1), category, rs2.getString(4));
+						}
 						addrs.add(addressDTO);
 					}
-					System.out.println(addr);
+
+//					System.out.println(category);
 				}
 //				System.out.println(addrs);
 				member.setAddress(addrs);
@@ -99,8 +100,7 @@ public class MemberDAO {
 				sql = "select * from haco_hashtag where h_m_pk=" + rs.getString(1);
 				pstmt = con.prepareStatement(sql);
 				rs3 = pstmt.executeQuery();
-				
-				
+
 				hashTag = new ArrayList<HashTagDTO>();
 				while (rs3.next()) {
 					// <a>트위터 해시태그 페이지로 넘어가기 위해 '#','＃'을 '%23'으로 replace
@@ -110,13 +110,13 @@ public class MemberDAO {
 					} else if (replaceHashTag.contains("＃")) {
 						replaceHashTag = replaceHashTag.replace("＃", "%23");
 					}
-					hashTagDTO = new HashTagDTO(rs3.getString(1),rs3.getString(3),replaceHashTag);
+					hashTagDTO = new HashTagDTO(rs3.getString(1), rs3.getString(3), replaceHashTag);
 					hashTag.add(hashTagDTO);
 				}
-				
+
 				member.setHashTag(hashTag);
 				members.add(member);
-				
+
 			}
 //			System.out.println("~~~~~~~~~~~~~~~~~");
 //			System.out.println(members);
@@ -131,7 +131,4 @@ public class MemberDAO {
 		}
 	}
 
-
-
-	
 }
--- src/main/webapp/member/member.jsp
+++ src/main/webapp/member/member.jsp
@@ -8,7 +8,9 @@
 <!-- Google font -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link
+	href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+	rel="stylesheet">
 
 <link rel="stylesheet" href="css/member.css" />
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
@@ -35,43 +37,44 @@
 					<div class="member-detail-container">
 						<div class="member-detail">
 							<div id="member-name">${m.m_name }</div>
-							<div id="member-birth"><span>お誕生日&nbsp;</span><span>${m.m_birth }</span></div>
-							<div id="member-debut"><span>デビュー日&nbsp;</span><span>${m.m_debut }</span></div>
-							<!-- 상세보기 (위치 수정 필요)-->
-							<!-- 							<details>
-								<summary>더보기..</summary> -->
-							<div class="member-detail2-box">
-								<div class="member-introduce-box">
-									<div id="member-introduce">
-										${m.m_introduce }</div>
-								</div>
-								<div class="member-mother-box">
-									<div id="member-mother-name">
-									<span>彼女のママは、</span>
-									<span><a href="${m.m_mother_twitter }">${m.m_mother_name }</a></span>
-<%-- 									<a href="${m.m_mother_twitter }" id="member-mother-twitter">
-									twitter</a> --%>
-									</div>
-								</div>
-								<div class="member-hashtag-container">
-									<div id="member-hashtag-title">-ハッシュタグ-</div>
-									<div class="member-hashtag-box">
-										<c:forEach items="${m.hashTag }" var="h">
-											<div id="member-hashtag">
-												<a href="https://x.com/search?q=${h.h_tag }">
-													${h.h_category }</a>
-											</div>
-										</c:forEach>
-									</div>
-								</div>
+							<div id="member-birth">
+								<span>お誕生日&nbsp;</span><span>${m.m_birth }</span>
 							</div>
-							<!-- 							</details> -->
+							<div id="member-debut">
+								<span>デビュー日&nbsp;</span><span>${m.m_debut }</span>
+							</div>
+							<!-- 상세보기 (위치 수정 필요)-->
+							<details>
+								<summary> 더보기..</summary>
+								<div class="member-detail2-box">
+									<div class="member-introduce-box">
+										<div id="member-introduce">${m.m_introduce }</div>
+									</div>
+									<div class="member-mother-box">
+										<div id="member-mother-name">
+											<span>彼女のママは、</span>
+											<span><a href="${m.m_mother_twitter }">${m.m_mother_name }</a></span>
+										</div>
+									</div>
+									<div class="member-hashtag-container">
+										<div id="member-hashtag-title">-ハッシュタグ-</div>
+										<div class="member-hashtag-box">
+											<c:forEach items="${m.hashTag }" var="h">
+												<div id="member-hashtag">
+													<a href="https://x.com/search?q=${h.h_tag }">
+														${h.h_category }</a>
+												</div>
+											</c:forEach>
+										</div>
+									</div>
+								</div>
+							</details>
 							<!-- 상세보기 끝 -->
 							<div class="member-address-container">
 								<div id="member-address-title">-アドレス-</div>
 								<div class="member-address-box">
 									<c:forEach items="${m.address }" var="a">
-										<div id="member-address">
+										<div id="${a.a_category }">
 											<a href="${a.a_address }">${a.a_category }</a>
 										</div>
 									</c:forEach>
