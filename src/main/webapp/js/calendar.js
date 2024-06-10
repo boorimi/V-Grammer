39f6983660ab7b593e0c8147b9c58a18a1a1474a
caru0316 <caru0316>
2024-06-10 17:11:10 +0900

    [delete] fullcalendar 기본탑재 이벤트 삭제

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -88,63 +88,7 @@ function loadCalendar() {
 			}
 			calendar.unselect()
 		},
-		// 이벤트 
-		events: [
-			{
-				title: 'All Day Event',
-				start: '2021-07-01',
-			},
-			{
-				title: 'Long Event',
-				start: '2021-07-07',
-				end: '2021-07-10'
-			},
-			{
-				groupId: 999,
-				title: 'Repeating Event',
-				start: '2021-07-09T16:00:00'
-			},
-			{
-				groupId: 999,
-				title: 'Repeating Event',
-				start: '2021-07-16T16:00:00'
-			},
-			{
-				title: 'Conference',
-				start: '2021-07-11',
-				end: '2021-07-13'
-			},
-			{
-				title: 'Meeting',
-				start: '2021-07-12T10:30:00',
-				end: '2021-07-12T12:30:00'
-			},
-			{
-				title: 'Lunch',
-				start: '2021-07-12T12:00:00'
-			},
-			{
-				title: 'Meeting',
-				start: '2021-07-12T14:30:00'
-			},
-			{
-				title: 'Happy Hour',
-				start: '2021-07-12T17:30:00'
-			},
-			{
-				title: 'Dinner',
-				start: '2021-07-12T20:00:00'
-			},
-			{
-				title: 'Birthday Party',
-				start: '2021-07-13T07:00:00'
-			},
-			{
-				title: 'Click for Google',
-				url: 'http://google.com/', // 클릭시 해당 url로 이동
-				start: '2021-07-28'
-			}
-		]
+	
 	});
 	// 캘린더 랜더링
 	calendar.render();
