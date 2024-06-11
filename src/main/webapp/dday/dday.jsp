716de240164a66588347908fb1268e86568444ce
caru0316 <caru0316>
2024-06-11 15:55:11 +0900

    [edit] 중간커밋

--- src/main/webapp/dday/dday.jsp
+++ src/main/webapp/dday/dday.jsp
@@ -4,33 +4,31 @@
 <html>
 <head>
 <title>D-Day and Contact List</title>
-</head>
 <link rel="stylesheet" href="css/dday.css">
-<body>
-	<div class="container">
-		<!-- 캘린더로 돌아가는 버튼 -->
-		<button id="calendarButton" class="button-15">캘린더로 돌아가기</button>
-
-		<h1>멤버의 디데이 목록</h1>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>이름</th>
-					<th>날짜</th>
-					<th>디데이까지 남은 일수</th>
-				</tr>
-			</thead>
-			<tbody>
-				<c:forEach var="dday" items="${ddayList}">
-					<tr>
-						<td>${dday.m_name}</td>
-						<td>${dday.m_debut}</td>
-						<td>${dday.daysUntilDday}</td>
-					</tr>
-				</c:forEach>
-			</tbody>
-		</table>
-	</div>
+</head>
+<body class="html-body">
+    <div class="container">
+        <button id="calendarButton" class="button-15">캘린더로 돌아가기</button>
+        <h1>멤버의 디데이 목록</h1>
+        <table class="table table-bordered" id="dday">
+            <thead>
+                <tr>
+                    <th>이름</th>
+                    <th>날짜</th>
+                    <th>디데이까지 남은 일수</th>
+                </tr>
+            </thead>
+            <tbody>
+                <c:forEach var="dday" items="${ddayList}">
+                    <tr>
+                        <td>${dday.m_name}</td>
+                        <td>${dday.m_debut}</td>
+                        <td>${dday.daysUntilDday}</td>
+                    </tr>
+                </c:forEach>
+            </tbody>
+        </table>
+    </div>
 <script src="js/dday.js"></script>
 </body>
 </html>
---
+++ src/main/webapp/js/calendar-old.js
@@ -0,0 +1,67 @@
+$(function() {
+    loadCalendar();
+    setTimeout(callBirth, 1000);
+});
+
+function callBirth() {
+    let title = $('.fc-toolbar-title');
+    let text = $(title).text();
+    let month = text.charAt(5);
+
+    $.ajax({
+        url: 'CalendarEventC',
+        data: { month },
+    })
+    .done(function(resData) {
+        setBirth(resData);
+    });
+}
+
+function setBirth(resData) {
+    const days = document.querySelectorAll(".fc-daygrid-day-number");
+    const div = document.createElement('div');
+    div.style.position = 'absolute';
+    days.forEach((day) => {
+        const date = JSON.parse(day.getAttribute("data-navlink"));
+        let top = 0;
+        let idx = 0;
+        resData.forEach((obj) => {
+            if (date.date == obj.debut) {
+                console.log(idx += 16)
+                const debutDiv = day.parentNode.parentNode.appendChild(div.cloneNode(true));
+                debutDiv.innerText = obj.name;
+                debutDiv.style.top = parseInt(idx) + 17 + 'px';
+            }
+        });
+    });
+};
+
+function loadCalendar() {
+    var calendarEl = $('#calendar')[0];
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        height: '700px',
+        expandRows: true,
+        slotMinTime: '08:00',
+        slotMaxTime: '20:00',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth'
+        },
+        initialView: 'dayGridMonth',
+        navLinks: true,
+        editable: false,
+        selectable: false, // 날짜 선택 비활성화
+        nowIndicator: true,
+        dayMaxEvents: true,
+        locale: 'ja',
+    });
+    calendar.render();
+}
+		$(document).ready(function() {
+			// 버튼 클릭 이벤트 핸들러
+			$('#ddayButton').click(function() {
+				// DdayC 서블릿으로 이동
+				window.location.href = 'DdayC';
+			});
+		});
