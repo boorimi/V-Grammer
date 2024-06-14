e6f682ad75d9cd52d5b8158b15aa2c0b23091b59
ARONA <haren3415@gmail.com>
2024-06-14 15:43:17 +0900

    [update] 마이페이지 브로마이드 정보 보기 기능 완성

--- src/main/java/com/vg/ds/trade/Test.java
+++ src/main/java/com/vg/ds/trade/Test.java
@@ -1,4 +1,4 @@
-package com.vg.jw.mypage;
+package com.vg.ds.trade;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,28 +7,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import com.google.gson.Gson;
 import com.vg.ignore.DBManager;
-import com.vg.jw.AccountDTO;
+import com.vg.jw.mypage.GoodsDTO;
 
-public class MyPageDAO {
+public class Test {
+	public static void main(String[] args) {
 
-	public static void getBromide(HttpServletRequest request) {
-		
 		System.out.println("getBromide메서드 진입");
 
 		// 세션에서 현재 사용자 id정보를 받아옴
-		HttpSession twitterLoginSession = request.getSession();
-		
-		long twitterId = (long) twitterLoginSession.getAttribute("twitterId");
-		
-		//일단 멤버들 뿌려줄거 - 이 결과로 나오는건 멤버 테이블의 이름과, pk / 이미지 테이블의 icon. 총 3개의 컬럼이 나옴 
+
+		long twitterId = 1797433642438029312L;
+
 		String MemberSql = "select hm.m_pk, hm.m_name, hi.i_icon from haco_member hm, haco_image hi where hm.m_pk = hi.i_m_pk";
-		
-		//icon, 멤버이름, 멤버pk, 유저트위터id, 굿즈수량 등 실제로 필요한 정보들 - DTO에 모아서 정리할 값들임
 		String sql = "SELECT hg.*, hi.i_icon, hm.m_name from haco_goods hg, haco_member hm, haco_image hi WHERE hi.i_m_pk = hg.g_m_pk and hi.i_m_pk = hm.m_pk and g_u_t_id = ? and g_category ='bromide'";
 
 		Connection con = null;
@@ -45,27 +36,19 @@ public class MyPageDAO {
 			con = DBManager.connect();
 			pstmt = con.prepareStatement(MemberSql);
 			rs = pstmt.executeQuery();
-			while (rs.next()) { // MemberSql의 결과 모든 멤버를 데려왔으니 rs.next가 진행하는 만큼 = 25개 전부 생성 
+			while (rs.next()) { // 25개 생성
 				haco_member = new GoodsDTO();
-				haco_member.setG_m_pk(rs.getInt("m_pk"));
-				haco_member.setM_name(rs.getString("m_name"));
-				haco_member.setI_icon(rs.getString("i_icon"));
 				haco_member.setU_twitter_id(twitterId);
+				haco_member.setG_m_pk(rs.getInt(1));
+				haco_member.setM_name(rs.getString(2));
+				haco_member.setI_icon(rs.getString(3));
 				haco_member.setG_category("bromide");
 				haco_members.add(haco_member);
 				hacoMemberMap.put(rs.getInt(1), haco_member); // Map에 추가
 			}
-			
-			System.out.println("[베이스가 되는 멤버 배열]");
-			for (GoodsDTO a : haco_members) {
-				System.out.println(a);
-			}
-			
 			pstmt.close();
 			rs.close();
-			
 
-			//해당 id의 굿즈가 몇개 있는지 보기 위해 twitterId로 굿즈 테이블에서 멤버pk와 갯수를 뽑아옴
 			pstmt = con.prepareStatement(sql);
 			pstmt.setLong(1, twitterId);
 			rs = pstmt.executeQuery();
@@ -74,13 +57,11 @@ public class MyPageDAO {
 				int g_m_pk = rs.getInt("g_m_pk");
 				int g_count = rs.getInt("g_count");
 
-				//위에서 전체 돌린 Map에 굿즈 테이블의 멤버pk(실제로 굿즈가 등록된 멤버)가 포함된 부분이 있는지 확인하고 그 부분의 카운트를 굿즈 테이블의 수치로 굿즈 카운트를 업데이트.
 				if (hacoMemberMap.containsKey(g_m_pk)) {
 					haco_member = hacoMemberMap.get(g_m_pk);
 					haco_member.setG_count(g_count); // g_count 업데이트
 				}
 			}
-			System.out.println("[업데이트한 멤버 배열]");
 			for (GoodsDTO a : haco_members) {
 				System.out.println(a);
 			}
@@ -91,7 +72,7 @@ public class MyPageDAO {
 //				bromideInfo = new GoodsDTO(rs.getInt("g_m_pk"), rs.getString("g_category"), rs.getInt("g_count"), rs.getString("i_icon"), rs.getString("m_name"));
 //				bromideInfos.add(bromideInfo);	// 25개로 만들기
 //			}
-			request.setAttribute("bromideInfos", haco_members);
+//			request.setAttribute("bromideInfos", bromideInfos);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -99,9 +80,5 @@ public class MyPageDAO {
 			DBManager.close(con, pstmt, rs);
 		}
 
-		
-		
-		
 	}
-
 }
--- src/main/webapp/account/mypage/mypage_goods.jsp
+++ src/main/webapp/account/mypage/mypage_goods.jsp
@@ -13,17 +13,54 @@
 	crossorigin="anonymous"></script>
 <script src="js/mypage_goods.js"></script>
 <script type="text/javascript">
-$(function() {
-	$("#goods-button-bromide").click(function() {
-		$("#bromide-content").slideToggle();
-		
-	});
-});
+	$(function() {
+		$("#goods-button-bromide").click(function() {
+			$("#bromide-content").slideToggle();
 
+		});
+	});
 </script>
+<style type="text/css">
+.mypage-goods-container {
+	position: relative;
+	width: 50vw;
+}
+
+.bromide-wrap {
+	display: flex;
+	justify-content: center;
+	width: 180px;
+}
+
+#bromide-content {
+	display: flex;
+	width: 100%;
+	justify-content: center;
+	flex-wrap: wrap;
+	gap: 20px 3px;
+}
+
+.bromide-wrap>div {
+	text-align: center;
+}
+
+.bromide-wrap>div:nth-child(1) {
+	width: 10%;
+}
+
+.bromide-wrap>div:nth-child(2) {
+	text-align: left;
+	width: 70%;
+	padding-left: 5px;
+}
+
+.bromide-wrap>div:nth-child(3) {
+	width: 20%;
+}
+</style>
 </head>
-<% 
-	MyPageDAO.getBromide(request);
+<%
+MyPageDAO.getBromide(request);
 %>
 
 
@@ -32,16 +69,64 @@ $(function() {
 		<div>
 			<h2>グッズ管理</h2>
 		</div>
-			<button id="goods-button-bromide">브로마이드 정보 ▼</button>
-			<div id="bromide-content">
+		<button id="goods-button-bromide">브로마이드 정보 ▼</button>
+		<div id="bromide-content">
 			<c:forEach var="bromide" items="${bromideInfos }">
-				<div class="goods-info-bromide">${bromide.g_category}</div>
-				<div class="goods-info-bromide">${bromide.g_count}</div>
-				<div class="goods-info-bromide"> <img alt="" src="haco_img/icon/${bromide.i_icon}"> ${bromide.i_icon}</div>
-				<div class="goods-info-bromide">${bromide.m_name}</div>
-				<br>
+				<div>
+					<div class="bromide-wrap">
+						<div class="goods-info-bromide">
+							<img alt="" style="width: 50px;"
+								src="haco_img/icon/${bromide.i_icon}">
+						</div>
+						<div class="goods-info-bromide">${bromide.m_name}</div>
+						<div class="goods-info-bromide">
+							<select>
+								<optgroup label="${bromide.g_count}ea"></optgroup>
+								<c:forEach begin="0" end="10" var="i">
+									<c:choose>
+										<c:when test="${i != bromide.g_count}">
+											<option value="${i}">${i }</option>
+										</c:when>
+										<c:otherwise>
+											<option selected value="${i }">${i }</option>
+										</c:otherwise>
+									</c:choose>
+								</c:forEach>
+							</select>
+						</div>
+					</div>
+				</div>
 			</c:forEach>
-			</div>
+		</div>
+		<button id="goods-button-bromide"> ▼</button>
+		<div id="bromide-content">
+			<c:forEach var="bromide" items="${bromideInfos }">
+				<div>
+					<div class="bromide-wrap">
+						<div class="goods-info-bromide">
+							<img alt="" style="width: 50px;"
+								src="haco_img/icon/${bromide.i_icon}">
+						</div>
+						<div class="goods-info-bromide">${bromide.m_name}</div>
+						<div class="goods-info-bromide">
+							<select>
+								<optgroup label="${bromide.g_count}ea"></optgroup>
+								<c:forEach begin="0" end="10" var="i">
+									<c:choose>
+										<c:when test="${i != bromide.g_count}">
+											<option value="${i}">${i }</option>
+										</c:when>
+										<c:otherwise>
+											<option selected value="${i }">${i }</option>
+										</c:otherwise>
+									</c:choose>
+								</c:forEach>
+							</select>
+						</div>
+					</div>
+				</div>
+			</c:forEach>
+		</div>
 	</div>
 </body>
 </html>
