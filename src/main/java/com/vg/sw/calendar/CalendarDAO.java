6b18c7431e6349bba0775cb06da4426565c8a8d0
caru0316 <caru0316>
2024-06-17 18:04:15 +0900

    [EDIT] Dday 생일 표기 추가

--- src/main/java/com/vg/sw/calendar/CalendarDAO.java
+++ src/main/java/com/vg/sw/calendar/CalendarDAO.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -43,7 +42,6 @@ public class CalendarDAO {
                     event.setTitle(rs.getString("m_name"));
                     String startDate = dateFormat.format(rs.getDate("m_debut"));
                     event.setStart(startDate);
-                    event.setRrule(generateRrule(startDate, 20)); // 10년 반복 설정
                     events.add(event);
                 }
                 if (rs.getDate("m_birth") != null) {
@@ -52,7 +50,6 @@ public class CalendarDAO {
                     birthEvent.setTitle(rs.getString("m_name") + "の誕生日");
                     String birthDate = dateFormat.format(rs.getDate("m_birth"));
                     birthEvent.setStart(birthDate);
-                    birthEvent.setRrule(generateRrule(birthDate, 20)); // 10년 반복 설정
                     events.add(birthEvent);
                 }
             }
@@ -66,20 +63,6 @@ public class CalendarDAO {
             response.getWriter().print("[]"); // 에러 발생 시 빈 JSON 배열 응답
         } finally {
             DBManager.close(conn, pstmt, rs);
-        }
-    }
-
-    private static String generateRrule(String startDate, int years) {
-        Calendar cal = Calendar.getInstance();
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            cal.setTime(dateFormat.parse(startDate));
-            cal.add(Calendar.YEAR, years);
-            String untilDate = dateFormat.format(cal.getTime());
-            return "{\"freq\":\"yearly\", \"until\":\"" + untilDate + "\"}";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
         }
     }
 }
