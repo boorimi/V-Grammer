778be1b9776c2aa4660325dfa168404e6e521be5
JABIRAN <ds6951@naver.com>
2024-06-25 17:00:15 +0900

    commit

--- src/main/java/com/vg/kw/archive/ArchiveUpdateC.java
+++ src/main/java/com/vg/kw/archive/ArchiveUpdateC.java
@@ -29,7 +29,6 @@ public class ArchiveUpdateC extends HttpServlet {
 //		request.setAttribute("content", "archive/archiveupdate.jsp");
 //		request.getRequestDispatcher("index.jsp").forward(request, response);
 //		response.sendRedirect("ArchiveC");
-//		ArchiveDAO.getAnotherPage(request, response);
 
 	}
 
